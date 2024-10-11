e3aed0da6b73f4b92d29ee2b385bc1ae3a37c099
sairamrangaraju <62653653+sairamrangaraju@users.noreply.github.com>
2024-10-11 12:14:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Welcome to FLM - Deploying React Application using AWS Amplify
+          Edit <code>src/App.js</code> This is Sairam Rangaraju
         </p>
         <a
           className="App-link"
