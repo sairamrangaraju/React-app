3f10ad3e4102dbf69aef3b1dadb8fabfc791603c
sairamrangaraju <62653653+sairamrangaraju@users.noreply.github.com>
2025-05-12 22:19:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> Welcome to Crowd Connect Planner's
+                                       Allthings at one place!
         </p>
         <a
           className="App-link"
